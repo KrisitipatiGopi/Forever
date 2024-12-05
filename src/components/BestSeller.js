6c4823d2f70a61b2a0511816755513654603805a
GopiKristipati <gopikristipati223@gmail.com>
2024-12-05 11:57:44 +0530

    Deploy to gh-pages

---
+++ src/components/BestSeller.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { products } from "../Utils/assets/frontend_assets/assets";
+import LatestCard from "./LatestCard";
+
+const BestSeller = () => {
+  console.log(products);
+
+  // Filter products marked as bestsellers
+  const bestSellers1 = products.filter((item) => item.bestseller === true); // Assuming 'bestseller' is a boolean
+  const bestSellers = bestSellers1.slice(0,5);
+  console.log(bestSellers);
+
+  return (
+    <div className="mt-10 w-full">
+      <div className="flex flex-col justify-center items-center gap-3">
+        <h1 className="text-4xl">BestSeller Collection───</h1>
+        <p>
+          Discover the trendiest styles with our latest collection, curated just
+          for you!
+        </p>
+      </div>
+      <div className="flex flex-wrap justify-center gap-4 mt-6">
+        {bestSellers.map((card) => (
+          <div key={card.id}>
+            <LatestCard item={card} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BestSeller;
